2f2f952acb8cbb8cefdddfe13dde8aa0c39a3007
wajima <goo.hiro.w@gmail.com>
2023-04-25 20:20:23 +0900

    import and export syntax

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,9 +1,9 @@
-import { Component } from "./base-component.js";
-import { Validatable, validate } from "../util/validation.js";
-import { autobind } from "../decorators/autobind.js";
+import Cmp from "./base-component.js";
+import * as Validation from "../util/validation.js";
+import { autobind as Autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
 
-export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
   mandayInputElement: HTMLInputElement;
@@ -30,16 +30,16 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredManday = this.mandayInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     };
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     };
-    const mandayValidatable: Validatable = {
+    const mandayValidatable: Validation.Validatable = {
       value: +enteredManday,
       required: true,
       min: 1,
@@ -47,9 +47,9 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     };
 
     if (
-      !validate (titleValidatable) || 
-      !validate (descriptionValidatable) || 
-      !validate (mandayValidatable)
+      !Validation.validate (titleValidatable) || 
+      !Validation.validate (descriptionValidatable) || 
+      !Validation.validate (mandayValidatable)
     ) {
       alert('入力値が不正です。');
       return;
@@ -64,7 +64,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     this.mandayInputElement.value = '';
   }
 
-  @autobind
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
